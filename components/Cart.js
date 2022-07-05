2969d4d24ef9338343e3bbd50f0796db106c6122
limbaniafagalde <limbufagalde@gmail.com>
2022-07-05 15:50:29 +0200

    Creating our cart

---
+++ components/Cart.js
@@ -0,0 +1,45 @@
+import { useStateContext } from "../lib/context";
+import { CartWrapper, CartStyle, Card, CardInfo, EmptyStyle, Quantity } from "../styles/CartStyle";
+import {FaShoppingCart} from "react-icons/fa";
+import { AiFillPlusCircle, AiFillMinusCircle } from "react-icons/ai";
+
+export default function Cart () {
+
+    const { cartItems } = useStateContext();
+    return(
+        <CartWrapper>
+            <CartStyle>
+                {cartItems.length < 1 && (
+                    <EmptyStyle>
+                        <h1>You have more shopping to do</h1>
+                        <FaShoppingCart/>
+                    </EmptyStyle>
+                )}
+                {cartItems.length >= 1 && 
+                    cartItems.map((item) => {
+                        return(
+                            <Card>
+                                <img src={item.image.data.attributes.formats.thumbnail.url} alt={item.title} />
+                                <CardInfo>
+                                    <h3>{item.title}</h3>
+                                    <h3>{item.price}</h3>
+                                    <Quantity>
+                                        <span>Quantity</span>
+                                        <button>
+                                            <AiFillMinusCircle/>
+                                        </button>
+                                        <p>{item.quantity}</p>
+                                        <button>
+                                            <AiFillPlusCircle/>
+                                        </button>
+                                    </Quantity>
+                                </CardInfo>
+                            </Card>
+                        );
+                    }
+
+                )}
+            </CartStyle>
+        </CartWrapper>
+    )
+}
--- components/Nav.js
+++ components/Nav.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { FiShoppingBag } from "react-icons/fi";
 import { NavStyle, NavItems } from "../styles/NavStyle";
+import Cart from "./Cart";
 
 
 export default function Nav(){
@@ -13,6 +14,7 @@ export default function Nav(){
                     <h3>Cart</h3>
                 </div>
             </NavItems>
+            <Cart/>
         </NavStyle>
     )
 }

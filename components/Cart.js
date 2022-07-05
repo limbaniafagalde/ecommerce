1040e0ac6e2393d1423a8d97e43e6a7c5ce30657
limbaniafagalde <limbufagalde@gmail.com>
2022-07-05 16:01:06 +0200

    Toggling our cart

--- components/Cart.js
+++ components/Cart.js
@@ -5,10 +5,10 @@ import { AiFillPlusCircle, AiFillMinusCircle } from "react-icons/ai";
 
 export default function Cart () {
 
-    const { cartItems } = useStateContext();
+    const { cartItems, setShowCart } = useStateContext();
     return(
-        <CartWrapper>
-            <CartStyle>
+        <CartWrapper onClick={()=> setShowCart(false)}>
+            <CartStyle onClick={(e)=> e.stopPropagation()}>
                 {cartItems.length < 1 && (
                     <EmptyStyle>
                         <h1>You have more shopping to do</h1>
